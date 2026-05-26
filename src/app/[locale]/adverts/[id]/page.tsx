6f1d0c6097f483397cb3f121bcf789c257e81edf
vch08 <Vojtech.Chmelar@oakm.cz>
2026-05-26 08:54:13 +0200

    Email, mazani inzeratu

--- src/app/[locale]/adverts/[id]/page.tsx
+++ src/app/[locale]/adverts/[id]/page.tsx
@@ -1,23 +1,3 @@
-// import AdvertClient from "@/components/AdvertClient";
-
-// export default async function Page({ params }: { params: { locale: string; id: string } }) {
-//   const { locale, id } = params;
-
-//   console.log("DETAIL PAGE ID:", id);
-
-//   const res = await fetch(`http://localhost:3000/api/adverts/${id}`, {
-//     cache: "no-store",
-//   });
-
-//   if (!res.ok) {
-//     return <div>Listing not found</div>;
-//   }
-
-//   const advert = await res.json();
-
-//   return <AdvertClient advert={advert} locale={locale} />;
-// }
-
 import AdvertClient from "@/components/AdvertClient";
 
 export default async function Page({ params }: { params: Promise<{ locale: string; id: string }> }) {
--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,6 +1,8 @@
 import { defineRouting } from "next-intl/routing";
 import { LOCALE_CODES } from "@/types/locale";
 
+export type Locale = (typeof routing.locales)[number];
+
 export const routing = defineRouting({
   locales: LOCALE_CODES,
   defaultLocale: "cs",
